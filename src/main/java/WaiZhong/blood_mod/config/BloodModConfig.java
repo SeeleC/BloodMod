b198a6a016a564148933e4969df76f48cf638c32
WaiZhong <1352510038@qq.com>
2022-05-02 13:08:56 +0800

    添加对ModMenu的支持

--- src/main/java/WaiZhong/blood_mod/config/BloodModConfig.java
+++ src/main/java/WaiZhong/blood_mod/config/BloodModConfig.java
@@ -5,15 +5,15 @@ import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
 import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 
-@Config(name = "title.blood_mod.config")
-@Config.Gui.Background("minecraft:textures/block/stone.png")
+@Config(name = "blood_mod")
+//@Config.Gui.Background("minecraft:textures/block/stone.png")
 public class BloodModConfig implements ConfigData {
-
-    @Comment("blood_mod:describe.blood_mod.config.display_hud")
+    //@Comment("blood_mod.describe.display_hud")
+    @ConfigEntry.Category("general")
     public boolean display_hud = true;
 
-    @ConfigEntry.Category("blood_mod:title.blood_mod.config.advanced")
+    @ConfigEntry.Category("advanced")
     public int hud_x = 0;
-    @ConfigEntry.Category("blood_mod:title.blood_mod.config.advanced")
+    @ConfigEntry.Category("advanced")
     public int hud_y = 0;
 }
